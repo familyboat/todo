4018be54dac7d1a6c7d55b3a40cd0b5ed42ce6ba
familyboat <2015301110129@whu.edu.cn>
2025-10-18 11:14:34 +0800

    release 1.0

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,24 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('to-do');
+  protected readonly todoApps: Array<{
+    title: string,
+    url: string,
+    status: '已完成' | '正在制作中' | '被迫中止'
+  }> = [
+    {
+      title: 'vue todo 应用',
+      url: 'https://familyboat.github.io/todo-vue/',
+      status: '已完成',
+    },
+    {
+      title: 'angular todo 应用',
+      url: 'https://familyboat.github.io/todo-angular/browser/',
+      status: '正在制作中',
+    }
+  ];
 }
